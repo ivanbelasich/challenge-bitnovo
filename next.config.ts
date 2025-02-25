a9d0acde09f4d2b4bdc17295c3834f2ba66916dc
Ivan <ivanbelasich@gmail.com>
2025-02-25 20:55:42 -0300

    Add Next.js configuration for remote image handling

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.qrserver.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "payments.pre-bnvo.com",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
