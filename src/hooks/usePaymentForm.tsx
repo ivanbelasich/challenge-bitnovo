10ac5758f4806cf403b6b5078832729fd5ffdd7e
Ivan <ivanbelasich@gmail.com>
2025-02-26 03:16:31 -0300

    Update payment creation form handling

--- src/hooks/usePaymentForm.tsx
+++ src/hooks/usePaymentForm.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import axios from 'axios';
 import { ICurrency } from '@/types/currency';
-import { API_BASE_URL } from '@/constants/api';
+import { ICreatePaymentRequest } from '@/types/payment';
+import { createPayment } from '@/services/paymentService';
 
 export const usePaymentForm = (currencies: ICurrency[]) => {
     const [amount, setAmount] = useState('');
@@ -17,6 +17,7 @@ export const usePaymentForm = (currencies: ICurrency[]) => {
         if (isNaN(numericValue)) {
             setAmount('');
             setAvailableCurrencies(currencies);
+            setSelectedCurrency('');
             return;
         }
         setAmount(value);
@@ -39,34 +40,16 @@ export const usePaymentForm = (currencies: ICurrency[]) => {
             return;
         }
 
-        if (['XRP', 'XLM', 'ALGO'].includes(selectedCurrency) && !tagMemo) {
-            setSubmitError('Por favor, ingresa el TAG/MEMO.');
-            setIsSubmitting(false);
-            return;
-        }
-
-        const formData = new FormData();
-        formData.append('expected_output_amount', amount);
-        formData.append('input_currency', selectedCurrency);
-        formData.append('merchant_urlko', 'https://webhook.site/error');
-        formData.append('merchant_urlok', 'https://webhook.site/success');
-        formData.append('notes', concept);
-        formData.append('reference', 'Referencia de pago');
-        formData.append('fiat', 'EUR');
-        formData.append('language', 'ES');
-
-        if (['XRP', 'XLM', 'ALGO'].includes(selectedCurrency)) {
-            formData.append('tag_memo', tagMemo);
-        }
-
         try {
-            const response = await axios.post(`${API_BASE_URL}/orders/`, formData, {
-                headers: { 'X-Device-Id': '4ca42f96-c3e7-47c5-803b-d87699552538' },
-            });
+            const paymentRequest: ICreatePaymentRequest = {
+                expected_output_amount: parseFloat(amount),
+                input_currency: selectedCurrency,
+                notes: concept,
+            };
 
-            window.location.href = `/payment/${response.data.identifier}`;
+            const { identifier } = await createPayment(paymentRequest)
 
-
+            window.location.href = `/payment/${identifier}`;
         } catch (error) {
             console.error('Error al crear la orden:', error);
             setSubmitError('Error al crear la orden. Inténtalo de nuevo.');
